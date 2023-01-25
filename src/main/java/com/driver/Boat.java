53a9b5ab4da3602d3e9cb5763ca51db508e74766
SRINIVASAN N R <srinivasannr99@gmai.com>
2023-01-25 12:05:07 +0530

    vehicl3

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,10 @@ package com.driver;
 public class Boat implements WaterVehicle{
     String name;
     int capacity;
+    Boat(String Name,int Capacity){
+        this.name=Name;
+        this.Capacity=capacity;
+    }
     public String getVehicleName(){
         return name;
     }
